a0677b9f9109762b99776242b0ccc69163d84171
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2023-12-18 11:38:51 -0300

    feat: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,23 @@
 import type { Metadata } from "next";
+import "./globals.css";
+import "tailwindcss/tailwind.css";
 import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Bebas from "next/font/local";
 
 export const metadata: Metadata = {
   title: "MoviesLib",
   description: "",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const bebas = Bebas({
+  src: "./../../fonts/bebas-neue.ttf",
+  variable: "--font-bebas",
+});
 
 export default function RootLayout({
   children,
@@ -14,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={`${inter.variable} ${bebas.variable}`}>{children}</body>
     </html>
   );
 }
